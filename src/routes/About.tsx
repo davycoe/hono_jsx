d80bb6200d598e7193e2cb718202edbd5da5ad42
davycoe <git@davycoe.com>
2023-11-22 23:00:08 +0000

    test

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,10 @@
+import Helmet from "preact-helmet";
+import { Link } from "preact-router/match";
+
 export const About = () => (
   <>
+    <Helmet title="Preact About Page!" />
     <h1>About!</h1>
+    <Link href="/">Home</Link>
   </>
 );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,11 @@
+import { Link } from "preact-router/match";
+
+import Helmet from "preact-helmet";
+
 export const Home = () => (
   <>
+    <Helmet title="Preact Home" />
     <h1>HOME!</h1>
+    <Link href="/about">About</Link>
   </>
 );
