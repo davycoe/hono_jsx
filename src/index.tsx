752e79f92d14acdbd5193899c1d1c05e47ec4e1b
davycoe <git@davycoe.com>
2023-11-22 11:18:27 +0000

    playing with jsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,28 @@ import { renderer } from "./renderer";
 
 const app = new Hono();
 
+export const View = (props: any) => (
+  <h2 style={props.color}>Hello, Cloudflare Pages from Hono JSX!</h2>
+);
+
 app.get("*", renderer);
 
 app.get("/", (c) => {
-  return c.render(<h1>Hello, Cloudflare Pages!</h1>);
+  return c.render(
+    <>
+      <View color="color:purple" />
+      <a href="/123">purple</a>
+    </>
+  );
+});
+
+app.get("/123", (c) => {
+  return c.render(
+    <>
+      <View color="color:red" />
+      <a href="/">red</a>
+    </>
+  );
 });
 
 export default app;
